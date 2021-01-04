d4b827231dafc4e078c45e91e5e7bafab9476210
Er. Krishna K Bhosle <73706904+KrishnaBhosle2225@users.noreply.github.com>
2021-01-04 22:33:54 +0530

    Update Creating_Table.java

--- Creating_Table.java
+++ Creating_Table.java
@@ -5,6 +5,11 @@ public class Creating_Table {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver Loaded....");
+		   }
+		
 	}
 
 }
